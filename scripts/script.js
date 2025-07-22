2cd8e1c0de90fdea6d4b93b4e601b7f889c6a831
NotAlex575 <dapontealessandro89gmail.com>
2025-07-22 23:45:46 +0200

    creata chiamata ajax per ottenere 6 elementi url immagini

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-img_and_description = document.getElementById("immagine-e-descrizione")
+//chiamata immagine-e-descrizione
+
+inizializzazione_colonne();
+
+
+function inizializzazione_colonne(){
+    axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then(response => {
+        for(let i = 0; i < 6; i++){
+            const immagini = response.data;
+            const url = immagini[i].url;
+            console.log(url);
+        }
+    })
+}
