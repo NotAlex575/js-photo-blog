d7cb715408ad53896ac85335f6e0e07a5603c974
NotAlex575 <dapontealessandro89gmail.com>
2025-07-23 00:47:59 +0200

    creato iterazione button che rimuove il d-none sulle 6 immagini e la aggiunge nella immagine grande

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 const img_and_description = document.getElementById("immagine-e-descrizione");
+const img_and_button = document.getElementById("immagine-singola-e-pulsante");
+const image_cliccable = document.getElementById("image-cliccable");
+const button_image = document.getElementById("one-photo"); 
 
 inizializzazione_colonne();
 
@@ -16,8 +19,14 @@ function inizializzazione_colonne(){
 const generateCard = ({url}) => {
     return `<div class="col-4">
             <div class="card bg-light border-0">
-                <img class="card-img-top" src="${url}" alt="">
+                <img class="card-img-top" src="${url}" alt="" id="image-cliccable">
                 <p class="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, impedit incidunt eligendi consectetur aut quia ullam quod beatae rem repellat.</p>
             </div>
           </div>`
 }
+
+button_image.addEventListener("click", function(){
+    img_and_description.classList.remove("d-none");
+    img_and_button.classList.add("d-none")
+})
+
