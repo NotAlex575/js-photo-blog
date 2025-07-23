64f053c60c179c70ab2b645250f1349ac695038b
NotAlex575 <dapontealessandro89gmail.com>
2025-07-23 10:44:10 +0200

    modificata immagine grande singola, in modo tale che in base all'immagine cliccata (che troviamo grazie all'array urlSearch) viene messa quell'immagine specifica

--- scripts/script.js
+++ scripts/script.js
@@ -9,20 +9,24 @@ inizializzazione_colonne();
 //INIZIALIZZAZIONE DELLE 6 CARDc(PASSAGGIO DEI VALORI)
 function inizializzazione_colonne(){
     axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then(response => {
+        let idSearch = [];
+        let urlSearch = [];
+        const immagini = response.data;
         for(let i = 0; i < 6; i++){
-            const immagini = response.data;
-            const url = immagini[i].url;
+            urlSearch.push(immagini[i].url);
+            idSearch.push(immagini[i].id);
+            console.log (idSearch)
             //GENERAZIONE CARD
-            img_and_description.innerHTML += generateCard({url});
+            img_and_description.innerHTML += generateCard({id: immagini[i].id, url: immagini[i].url});
         }
 
         //questo perchè prima si genera la card, e poi puoi richiamare tutto il contenuto con l'id (che si richiama con #) "image-cliccable"
         const immagini_cliccabili = document.querySelectorAll("#image-cliccable");
         //per ogni button immagine creata in precedenza
-        immagini_cliccabili.forEach(button => {
+        immagini_cliccabili.forEach((button, counter) => {
             button.addEventListener("click", function(){
                 img_and_button.classList.remove("d-none");
-                immagine_grande.innerHTML += `<img class="card-img-top" src="img/Harleq.jpg" alt=""></img>`
+                immagine_grande.innerHTML += `<img class="card-img-top" src="${urlSearch[counter]}" alt=""></img>`
                 img_and_description.classList.add("d-none");
             });
         });
@@ -30,11 +34,11 @@ function inizializzazione_colonne(){
 }
 
 //CREAZIONE CARD (UNO AD UNO)
-const generateCard = ({url}) => {
+const generateCard = ({id, url}) => {
     return `<div class="col-4">
-            <div class="card bg-light border-0">
+            <div class="card border-0">
                 <button type="button" class="border-0" id="image-cliccable">
-                    <img class="card-img-top" src="${url}" alt="">
+                    <img class="card-img-top" id="${id}" src="${url}" alt="">
                 </button>
                 <p class="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, impedit incidunt eligendi consectetur aut quia ullam quod beatae rem repellat.</p>
             </div>
